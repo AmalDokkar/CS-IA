749b05c3d7aa19e1874c0185ce105b84de9882a2
AmalDokkar <amaldok@gmail.com>
2022-11-15 10:33:14 +0100

    update

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -59,14 +59,17 @@ class Interpreter():
 	def interp_recognize(self):
 		if self.global_status == 'paused':
 			return
-		# file = AudioData('rec.wav')
-		# with file as source:
+		
+		with sr.AudioFile("/home/amaldok/Prog/CS-IA/tests/rec.wav") as source:
 		
 		# for linux set device_index = to default
-		with sr.Microphone(sample_rate=44100) as source:
+		# with sr.Microphone(sample_rate=44100) as source:
 			try:
 				print('Speak now') # Send signal
-				audio = self.recognizer.listen(source, timeout=10, phrase_time_limit=30)
+				# For microphone
+				# audio = self.recognizer.listen(source, timeout=10, phrase_time_limit=30)
+				# For file
+				audio = self.recognizer.record(source)
 				text = self.recognizer.recognize_google(audio, language=self.src_lang)
 			except sr.WaitTimeoutError:
 				print('WaitTimeoutError')
@@ -101,16 +104,17 @@ class Interpreter():
 		time.sleep(1)
 
 	def display_spoken_text(self):
-		srcBuffer = Gtk.EntryBuffer()
-		srcBuffer.insert_text(0, self.transcribed_text, len(self.transcribed_text))
-		srcEntry = self.builder.get_object("SpokenTextEntry")
-		srcEntry.set_buffer(srcBuffer)
+		srcBuffer = Gtk.TextBuffer()
+		i = srcBuffer.get_start_iter()
+		srcBuffer.do_insert_text(srcBuffer, i, self.transcribed_text, len(self.transcribed_text))
+		srcTextView = self.builder.get_object("SpokenTextBuffer")
+		srcTextView.set_buffer(srcBuffer) # ara falla aixo
 
 	def display_translated_text(self):
-		destBuffer = Gtk.EntryBuffer()
-		destBuffer.insert_text(0, self.translated_text, len(self.translated_text))
-		destEntry = self.builder.get_object("TranslatedTextEntry")
-		destEntry.set_buffer(destBuffer)
+		destBuffer = Gtk.TextBuffer()
+		destBuffer.do_insert_text(0, self.translated_text, len(self.translated_text))
+		destTextView = self.builder.get_object("TranslatedTextBuffer")
+		destTextView.set_buffer(destBuffer)
 
 # interpreter = Interpreter()
 # interpreter.set_src_lang('es')
